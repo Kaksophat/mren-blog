cae6cfe7d7ae91cd27f5af5ceb0350c758cf6853
kak sophat <kaksophatt@gmail.com>
2024-07-26 14:54:38 +0700

    connect db

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,15 @@
 import exress from "express"
-
+import mongoose from "mongoose";
+import  dotenv  from "dotenv";
+dotenv.config()
+mongoose
+.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log("db connect success");
+})
+.catch((err)=>{
+    console.log(console.error(err));
+})
 const app = exress()
 
 app.listen(3000,()=>{

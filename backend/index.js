abcb6575a9e6712ea04a2f74068a3300566593ad
kak sophat <kaksophatt@gmail.com>
2024-07-26 11:26:45 +0700

    commit

---
+++ backend/index.js
@@ -0,0 +1,7 @@
+import exress from "express"
+
+const app = exress()
+
+app.listen(3000,()=>{
+    console.log("server is running on port 3000");
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+
+import { Routes,Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Signup from "./pages/Signup"
+import Signin from "./pages/Signin"
+import Dashboard from "./pages/Dashboard"
+import Project from "./pages/Project"
+import Header from "./components/Header"
+function App() {
+  
+
+  return (
+    <>
+    <Header />
+     <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/signup" element={<Signup/>}/>
+      <Route path="/signin" element={<Signin/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path="/project" element={<Project/>}/>
+     </Routes>
+    </>
+  )
+}
+
+export default App
